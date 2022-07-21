c939bed2f53e597d7a0d1565691540ed67d34fbf
Caique Evaristo <caique.evaristo@live.com>
2022-07-21 14:13:57 -0300

    Add Terraform step

--- wsl2-setup-script.sh
+++ wsl2-setup-script.sh
@@ -177,4 +177,20 @@ then
 	echo -e '\n# Enable kubectl completion using bash\nsource <(kubectl completion bash)' >> ~/.bashrc
 fi
 
+echo  " "
+read -p "Do you want to set up Terraform? [y/n] " terraformReply
+if [[ $terraformReply =~ ^[Yy]$ ]]
+then
+	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	
+	echo -e "\nHashicorp GPG key fingerprint:\n"
+	gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+	echo  " "
+	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+	https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+	echo $sudoPassword | sudo -S apt update && sudo -S apt install terraform -y
+	echo -e "\nVerifying terraform installation:\n"
+	terraform -version
+fi
+
 echo -e "\nDone!"
